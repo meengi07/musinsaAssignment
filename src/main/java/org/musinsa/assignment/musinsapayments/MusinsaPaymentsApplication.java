31aed6f621e0c1f321529a9135176359c97ab657
meengi <meengi07@gmail.com>
2024-09-24 19:39:18 +0900

    feat : project init

---
+++ src/main/java/org/musinsa/assignment/musinsapayments/MusinsaPaymentsApplication.java
@@ -0,0 +1,13 @@
+package org.musinsa.assignment.musinsapayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/musinsa/assignment/musinsapayments/MusinsaPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.musinsa.assignment.musinsapayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
